c5bf6c731496451726df1d5d5c3f73d4b8840699
Akinniyi Thomas <akinniyithomas@yahoo.com>
2019-07-29 03:08:30 +0100

    models in progress, the admin interface looks okay for now

--- EBC_Project/ebc_app/admin.py
+++ EBC_Project/ebc_app/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+#class SerieAdmin(admin.TabularInline):
+#    model = Serie
+#    extra = 2
+
+#class PreacherAdmin(admin.TabularInline):
+#    model = Preacher
+#    extra = 1
+
+#class TopicSectionAdmin(admin.TabularInline):
+#    model = TopicSection
+#    extra = 2
+
+class MessageAdmin(admin.ModelAdmin):
+    fieldsets=[(None,{'fields':['topic']}),
+            ('File URL', {'fields':['fileLink']}),
+            ('Date of Upload',{'fields':['dateAndTime']}),
+            ('Topic Sections', {'fields':['topicSection']}),
+            ('Preacher', {'fields':['preacher']}),
+            ('Message Series',{'fields':['series']})
+            ]
+    list_display = ['topic','preacher','series','fileLink','dateAndTime']
+    #inlines =[SerieAdmin, PreacherAdmin]
+admin.site.register(Message, MessageAdmin)
+admin.site.register(TopicSection)
+admin.site.register(Preacher)
+admin.site.register(Serie)
--- EBC_Project/ebc_app/models.py
+++ EBC_Project/ebc_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Serie(models.Model):
+    title = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.title
+
+class Preacher(models.Model):
+    name=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class TopicSection(models.Model):
+    section = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.section
+
+class Message(models.Model):
+    topic = models.CharField(max_length=200)
+    series = models.ForeignKey(Serie, on_delete=models.PROTECT)
+    fileLink = models.URLField(max_length=1000)
+    topicSection = models.ManyToManyField(TopicSection)
+    preacher = models.ForeignKey(Preacher, on_delete=models.PROTECT)
+    dateAndTime = models.DateTimeField('date uploaded')
+
+    def __str__(self):
+        return self.topic
