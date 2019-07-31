085d2beb182f6ea4f856881a596551eb7f247965
Akinniyi Thomas <akinniyithomas@yahoo.com>
2019-07-31 06:03:56 +0100

    still working on table with the models...i.e. template populating the models data

--- EBC_Project/ebc_app/views.py
+++ EBC_Project/ebc_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
+    mesages = Message.objects.order_by('pk')
     return render(request, "ebc_app/index.html", 
            {
                'title' : 'EBC YOUTH!!',
+               'mesages' : mesages,
            })
